54db383852ffd3f28dd2f27a4cb61c4560d63b7d
rcdamera <rcdamera@gmail.com>
2021-08-24 13:01:09 +0530

    this is Aug-24-sts-2

--- gitTest/src/gitTest/gitConnect.java
+++ gitTest/src/gitTest/gitConnect.java
@@ -3,8 +3,11 @@ package gitTest;
 public class gitConnect {
 
 	public static void main(String[] args) {
-		 System.out.println("Hello 5467");
-
+		//Ram changes for this commit 
+		System.out.println("Hello 5467");
+		 
+		 
+		System.out.println("Hello Ram finally");
 	}
 
 }
